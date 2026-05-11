c1eb71ef51a00a8141fe17a628cf7d3d85d2a1c6
Joshua Emorwodia <joshuaemorwodia@gmail.com>
2026-05-11 10:23:27 +0100

    first commit

---
+++ JS/loader.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",function(){
+
+setTimeout(function(){
+
+document.getElementById("loader").style.display="none";
+
+},10000);
+
+});
+function toggleMenu(){
+
+document.getElementById("nav").classList.toggle("active")
+
+}
